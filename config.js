10e15ee25530f66064b5c2b167326e0f056538de
Rowy <72692336+RowyHere@users.noreply.github.com>
2025-11-03 14:56:43 +0300

    for 'i'

--- config.js
+++ config.js
@@ -1,45 +1,31 @@
-const date_ = new Date();
-
-function isAvaible() {
-
-    if (date_.getMonth() > 5) {
-        return true
-    }
-    if (date_.getMonth() == 5 && date_.getDate() >= 5) {
-        return true;
-    }
-}
-
-module.exports = {
-    discord: {
-        id: "465646793401565186",
+const config = {
+ 
+    personalInfo: {
+        name: "Rowy",
+        title: "Full Stack Developer & UI/UX Designer",
+        bio: ` `,
     },
-    github: {
-        token1: "ghp_glp5HVMRtuOmz6b",
-        token2: "sQrU6hpJNdqo10J2IkmuF"
-    },
+    
     about: {
-        description: "Hi, my name is Yasin and I'm 18 yo. I started software development 3 year ago with developing Discord bots. And i continued to develop myself in the direction of html and javascript, i still learning new stuff in programming. I'm realizing my big projects right now, i'm a JavaScript, TypeScript, Python, CSS, HTML and Vue developer. he database it usually my uses is \"mongodb\". You can follow me for more information!",
+        description: `Hi, my name is Yasin Efe and I'm 20 yo. I started software development 5 year ago with developing Discord bots. And i continued to develop myself in the direction of html and javascript, i still learning new stuff in programming. I'm realizing my big projects right now, i'm a JavaScript, TypeScript, Python, CSS, HTML and Vue developer. he database it usually my uses is "mongodb". You can follow me for more information!`,
+        skills: [
+            "Frontend: React, Vue.js, TypeScript, HTML, CSS",
+            "Backend: Node.js, Python, C#",
+            "Database: MongoDB, SQL",
+            "Design: Figma, Photoshop",
+        ]
     },
-    social: {
+    
+    projects: [
+    ],
+    
+    socialLinks: {
         github: "https://github.com/RowyHere",
-        instagram: "https://instagram.com/rowyycim",
-        discord: "https://discord.com/users/465646793401565186",
-        spotify: "https://open.spotify.com/user/31zs2vzlyzsc37avl5b5cubrfgt4?si=c986c3af8dd64108",
+        spotify: "https://open.spotify.com/user/31zs2vzlyzsc37avl5b5cubrfgt4?si=7611c03a00d94de4",
+        instagram: "https://instagram.com/rowycik"
     },
-    meta: {
-        title: 'My Personal Website',
-        description: 'My Personal Website',
-        theme: '#02060f',
-        lang: 'en',
-        url: 'https://',
-        image: '',
-    },
-    title: 'Rowy',
-    titles: [
-        { title: 'Home', url: '/', icon: 'fa-house', invisible: false },
-        { title: 'Projects', url: '/projects', icon: 'fa-blog', invisible: false },
-        { title: 'contact', url: '/contact', icon: 'fa-envelope', invisible: true },
-    ]
-}
-
+    
+    contact: {
+        email: "contact@rowycik.app"
+    }
+};
